e6c2ef6c4f922d4dbcdd41e1da84166b9549f88e
danac4 <danac4@mail.tau.ac.il>
2024-02-01 22:53:30 +0200

    added logo and background color

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav>
       <div className="logo">
-        <Image src="/logo.png" alt="site logo" width={128} height={77} />
+        <Image src="/logo.png" alt="site logo" width={200} height={80} />
       </div>
       <Link href="/">Home</Link>
       <Link href="/about">About</Link>
