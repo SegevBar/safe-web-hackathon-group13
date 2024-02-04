955998607f09b487667b62ecc777f692d739fddc
danac4 <dana1997@windowslive.com>
2024-02-04 19:38:12 +0200

    Update index.js
    + added data in comment

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,31 @@ import Category from '../comps/Category'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  // const data = [
+  //   {
+  //     url: "https://www.youtube.com/watch?v=5Zjplzo5YZg",
+  //     title: "Commercial Criticism",
+  //     thumbnail: "https://i.ytimg.com/vi/5Zjplzo5YZg/maxresdefault.jpg",
+  //     views: "1.8M",
+  //     id: 1,
+  //   },
+  //   {
+  //     url: "https://www.youtube.com/watch?v=DBJVj2DLuLA",
+  //     title: "The 7 Habits of Highly Effective People",
+  //     thumbnail: "https://i.ytimg.com/vi/DBJVj2DLuLA/maxresdefault.jpg",
+  //     views: "1.8M",
+  //     id: 2,
+  //   },
+  //   {
+  //     url: "https://www.youtube.com/watch?v=TwWZSW2GkXY",
+  //     title: "Kupa Rashit",
+  //     thumbnail: "https://i.ytimg.com/vi/TwWZSW2GkXY/maxresdefault.jpg",
+  //     views: "1.8M",
+  //     id: 3,
+  //   },
+  // ];
+
+  
   const categories = [{id:1, categoryName: "Yoga", path: "/about", image:"/yogaLogo.png" },
   {id:2, categoryName: "Cook", path: "/about", image:"/cook2.png" },
   {id:3, categoryName: "Music", path: "/about", image:"/music.png" },
